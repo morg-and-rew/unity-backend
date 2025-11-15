99b434bfa4c95e671ddf778f1d7f9100edd73527
morg-and-rew <152051752+morg-and-rew@users.noreply.github.com>
2025-11-15 23:30:38 +0700

    Fix

--- src/db.php
+++ src/db.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 // Используем данные Render Managed Database
 $host = "dpg-d4c7lujipnbc7399d5sg-a";  // сюда Render даёт хост
 $user = "unity_game_db_user";              // имя пользователя Render
